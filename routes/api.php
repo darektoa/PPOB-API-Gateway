0fb44831e0dce788b57cfe8692d85a7e34e23cd0
darektoa <darektoa@gmail.com>
2021-11-22 22:23:12 +0700

    Added: Auth prefix in API route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('/auth')->group(function() {
+  Route::get('/login', []);
 });
