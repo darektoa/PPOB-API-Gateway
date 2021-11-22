e37bc12e2b3beaccbe5c506f465269b388ccc72d
darektoa <darektoa@gmail.com>
2021-11-22 23:46:40 +0700

    Added: Partner table and migration

---
+++ database/migrations/2021_11_22_164425_create_partners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->text('api_key');
+            $table->text('secret_key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partners');
+    }
+}
