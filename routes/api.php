8b940c688411cfc245c550c0250e667e5f0cdc0f
darektoa <darektoa@gmail.com>
2021-11-22 23:42:21 +0700

    Added: Test route for testing bukalapak helper token

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\BukalapakHelper;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
+
+Route::get('/test', function() {
+  return response()->json(['data' => BukalapakHelper::token()]);
+});
 
 Route::prefix('/auth')->group(function() {
   Route::post('/login', []);
