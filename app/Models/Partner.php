99bfd30899e983108c10d24edd1f52a83f87fd32
darektoa <darektoa@gmail.com>
2021-11-22 23:53:46 +0700

    Added: User model relationship in partner model

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Partner extends Model
 {
     use HasFactory;
+
+    protected $guarded  = ['id'];
+
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
 }
