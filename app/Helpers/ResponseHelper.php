c286026ba6c3c3274bb8433b61fb65f2e616964b
darektoa <darektoa@gmail.com>
2021-12-13 23:13:26 +0700

    Added: Error method in response helper

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -12,4 +12,13 @@ class ResponseHelper {
             'data'    => $data,
         ], $status);
     }
+
+
+    static public function error($errors=[], string $message='Failed', $status=500) {
+        return response()->json([
+            'status'    => $status,
+            'message'   => $message,
+            'errors'    => $errors,
+        ], $status);
+    }
 }
