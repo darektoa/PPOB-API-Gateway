80ed4b7c074006d40451811a90e8c77a3702cb4a
darektoa <darektoa@gmail.com>
2021-12-14 22:00:06 +0700

    Edited: Bukalapak base url to bukalapak account url in token method of bukalapak helper

--- app/Helpers/BukalapakHelper.php
+++ app/Helpers/BukalapakHelper.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Http;
 
 class BukalapakHelper{
   static public function token() {
-    $BASE_URL = env('BUKALAPAK_BASE_URL');
+    $BASE_URL = env('BUKALAPAK_ACCOUNT_URL');
     $endpoint = "$BASE_URL/oauth/token";
     $response = Http::post($endpoint, [
       'grant_type'    => 'client_credentials',
