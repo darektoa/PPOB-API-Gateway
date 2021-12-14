73fa98eda44919e5059b66ba3a125c0a2eadf080
darektoa <darektoa@gmail.com>
2021-12-14 23:29:18 +0700

    Added: Partner token in partner seeder

--- database/seeders/PartnerSeeder.php
+++ database/seeders/PartnerSeeder.php
@@ -10,10 +10,16 @@ class PartnerSeeder extends Seeder
 {
     public function run()
     {
-        Partner::create([
+        $partner = Partner::create([
             'name'          => 'EXAMPLE',
             'api_key'       => 'uQEbfJq6OSW9yPweuc7QlNd5qY5yJJ5sWBI3VDs65gQa',
             'secret_key'    => Hash::make('Kbb4MdCyNIBNI6x07hZSYjlm69dEBh8yvkJNPQX8rtUx'),
+        ]);
+
+        $partner->tokens()->create([
+            'name'      => 'auth_token',
+            'abilities' => '[*]',
+            'token'     => '09d3ad6a6554a8ed428e25a53fde4ac428790df8b3c4ddfa58c6ea17f94385fa'
         ]);
     }
 }
