5da2a24c63b0b25880b3ee8ee1ae1f2e93568843
darektoa <darektoa@gmail.com>
2021-12-16 05:32:58 +0700

    Added: get partner access token API route

---
+++ app/Http/Controllers/PartnerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\ErrorException;
+use App\Models\{Partner, PersonalAccessToken};
+use App\Helpers\ResponseHelper;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{Hash, Validator};
+
+class PartnerController extends Controller
+{
+    public function token(Request $request) {
+        try{
+            $validator  = Validator::make($request->all(), [
+                'api_key'       => 'required',
+                'secret_key'    => 'required'
+            ]);
+
+            if($validator->fails()){
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable', $errors, 422);
+            }
+
+            $apiKey     = $request->api_key;
+            $secretKey  = $request->secret_key;
+            $partner    = Partner::where('api_key', '=', $apiKey)->first();
+            
+            if(!$partner)
+                throw new ErrorException('Unprocessable', ["Api key doesn't exist"], 422);
+            if(!Hash::check($secretKey, $partner->secret_key))
+                throw new ErrorException('Unprocessable', ["Credential doesn't match"], 422);
+
+            $token = $partner->createToken('partner_token');
+
+            return ResponseHelper::make($token->plainTextToken);
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
+}
