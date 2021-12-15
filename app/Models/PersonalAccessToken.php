412508aff3d40289ff88b068c4ea1b328d0112fc
darektoa <darektoa@gmail.com>
2021-12-15 10:03:45 +0700

    Edited: Extends personal access tokens model from sanctum personal access token model

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
-class PersonalAccessToken extends Model
+class PersonalAccessToken extends SanctumPersonalAccessToken
 {
     use HasFactory;
 
