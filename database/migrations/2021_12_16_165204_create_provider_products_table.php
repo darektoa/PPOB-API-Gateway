57ab44e45e0c57d96de485c48e2c8a1cd293d7f1
darektoa <darektoa@gmail.com>
2021-12-16 23:54:35 +0700

    Added: Provider products table migration

---
+++ database/migrations/2021_12_16_165204_create_provider_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProviderProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('provider_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->foreignId('product_provider_id');
+            $table->string('name', 100);
+            $table->string('code', 50)->unique();
+            $table->bigInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('provider_products');
+    }
+}
