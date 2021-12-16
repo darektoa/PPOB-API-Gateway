febd35122388b14cb944de5c2cc1b12a58a6f6ac
darektoa <darektoa@gmail.com>
2021-12-16 22:51:09 +0700

    Added: Products table migration

---
+++ database/migrations/2021_12_16_143614_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_category_id');
+            $table->string('name', 100);
+            $table->string('code', 50);
+            $table->smallInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
