65d6619c82dd9138dd590de39036878e5695ba38
darektoa <darektoa@gmail.com>
2021-12-22 22:55:24 +0700

    Added: Index method in product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ResponseHelper;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function index() {
+        $products = Product::all();
+
+        return ResponseHelper::make($products);
+    }
 }
