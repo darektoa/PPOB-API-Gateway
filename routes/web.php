bdc9fc1860f9faf71501a05bc660db098af00b4d
darektoa <darektoa@gmail.com>
2021-12-22 22:55:51 +0700

    Added: Product list API route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function() {
     Route::post('/token', [PartnerController::class, 'token']);
 });
+
+
+// WITH PARTNER AUTHENTICATION
+Route::middleware('auth.partner')->group(function() {
+    // PRODUCT
+    Route::prefix('/products')->group(function() {
+        Route::get('/', [ProductController::class, 'index']);
+    });
+});
