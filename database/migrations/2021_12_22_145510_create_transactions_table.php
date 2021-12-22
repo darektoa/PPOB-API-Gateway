f775d61a40a6fd8b9fc891014dfe4bc5ce7bdeb4
darektoa <darektoa@gmail.com>
2021-12-22 22:05:39 +0700

    Added: Transactions table migration

---
+++ database/migrations/2021_12_22_145510_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('partner_id');
+            $table->foreignId('product_id')->nullable();
+            $table->string('order_id');
+            $table->bigInteger('amount');
+            $table->smallInteger('status');
+            $table->smallInteger('type');
+            $table->json('customer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
