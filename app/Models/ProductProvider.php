90879b5a987aa24be8d03c2e9919316392218b8c
darektoa <darektoa@gmail.com>
2021-12-24 20:16:03 +0700

    Added: Products relation method in product provider model

--- app/Models/ProductProvider.php
+++ app/Models/ProductProvider.php
@@ -10,4 +10,9 @@ class ProductProvider extends Model
     use HasFactory;
 
     protected $guarded  = ['id'];
+    
+
+    public function products() {
+        return $this->hasMany(ProviderProduct::class);
+    }
 }
