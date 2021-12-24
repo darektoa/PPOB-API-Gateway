d15c8149e0009a349d0036907d4430125ee4ae5e
darektoa <darektoa@gmail.com>
2021-12-24 21:30:05 +0700

    Added: Constructor in bukalapak controller

--- app/Http/Controllers/BukalapakController.php
+++ app/Http/Controllers/BukalapakController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class BukalapakController extends Controller
 {
-    //
+    protected $BASE_URL;
+
+    protected $request;
+
+    protected $product;
+
+    protected $providerProduct;
+    
+
+    public function __construct(Request $request, $product) {
+        $this->BASE_URL         = env('BUKALAPAK_API_URL');
+        $this->request          = $request;
+        $this->product          = $product;
+        $this->providerProduct  = $product
+            ->providerProducts()
+            ->whereRelation('provider', 'code', '=', 'BKLPK')
+            ->first();
+    }
 }
