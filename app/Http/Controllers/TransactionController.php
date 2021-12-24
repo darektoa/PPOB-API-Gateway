58fded1603cc3b9da9b4a677164442380c67e0e7
darektoa <darektoa@gmail.com>
2021-12-24 21:34:09 +0700

    Added: Request validator in store method of transaction controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,11 +6,24 @@ use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Models\{Product, Transaction};
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TransactionController extends Controller
 {
     public function store(Request $request) {
         try{
+            $validator = Validator::make($request->all(), [
+                'order_id'        => 'required|alpha_dash',
+                'product_code'    => 'required|exists:products,code',
+                'account_number'  => 'required|string',
+                'amount'          => 'nullable|numeric'
+            ]);
+            
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable', $errors, 422);
+            }
+
             $productCode  = $request->product_code;
             $product      = Product::with('category.setting.provider')->where('code', '=', $productCode)->first();
             $category     = $product->category;
