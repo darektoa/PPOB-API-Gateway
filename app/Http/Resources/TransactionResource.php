2b6f4a81f1cab6ba6dd775c9072a2d0944077a1b
darektoa <darektoa@gmail.com>
2021-12-27 10:32:45 +0700

    Added: Properties in transaction API resource

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TransactionResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
-            
+            'order_id'          => $this->order_id,
+            'account_number'    => $this->account_number,
+            'product_code'      => $this->product_code,
+            'product_name'      => $this->product_name,
+            'category'          => $this->category,
+            'amount'            => $this->amount ?? 0,
+            'token'             => $this->when($this->token, $this->token),
+            'admin_fee'         => $this->when($this->admin_fee, $this->admin_fee),
+            'status'            => $this->status,
         ];
     }
 }
